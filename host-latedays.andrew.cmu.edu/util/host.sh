eeb8687c7d3434bdbab75a6e9d9f234e760a4222
Jacob Zimmerman <zimmerman.jake@gmail.com>
2015-03-22 23:46:21 -0400

    Add host latedays

---
+++ host-latedays.andrew.cmu.edu/util/host.sh
@@ -0,0 +1,45 @@
+#
+# host.sh - Host-dependent configurations for "latedays.andrew.cmu.edu"
+#
+# Description of file.
+#
+# Author
+#   Jake Zimmerman <jake@zimmerman.io>
+#
+# Usage
+#
+# Notes
+#
+# TODOs
+
+# ----- PATH, MANPATH, and LD_LIBRARY_PATH ------------------------------------
+
+# Computer Club contributed software
+export PATH="/afs/club/contrib/bin:$PATH";
+export MANPATH="$MANPATH:/afs/club.cc.cmu.edu/contrib/man"
+
+export MANPATH="$MANPATH:$HOME/share/man"
+
+# ----- aliases ---------------------------------------------------------------
+
+# Git aliases
+alias gl='git log --graph --pretty="%C(bold green)%h%Creset%C(blue)%d%Creset %s"'
+alias gla='git log --graph --pretty="%C(bold green)%h%Creset %C(yellow)%an%Creset%C(blue)%d%Creset %s"'
+
+# ----- Prompt  ---------------------------------------------------------------
+
+PROMPT_PURE_DIR_COLOR="%F{green}"
+
+# ----- Miscellaneous ---------------------------------------------------------
+
+# Don't let people write messages to me
+mesg n
+
+# update-host
+# Usage:
+#     update-host
+#
+# You should never have to call this. It's called when you call `update`
+update-host () {
+  # nothing host-specific to update!
+}
