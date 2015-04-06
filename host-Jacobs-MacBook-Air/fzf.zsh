9326e85f7c255faab2f03abd56ce4054fbc56560
Jacob Zimmerman <zimmerman.jake@gmail.com>
2015-04-06 03:36:26 -0400

    Update fzf

--- host-Jacobs-MacBook-Air/fzf.zsh
+++ host-Jacobs-MacBook-Air/fzf.zsh
@@ -1,65 +1,20 @@
 # Setup fzf
 # ---------
-unalias fzf 2> /dev/null
-unset fzf 2> /dev/null
-if [[ ! "$PATH" =~ "/usr/local/Cellar/fzf/0.9.4-1/bin" ]]; then
-  export PATH="/usr/local/Cellar/fzf/0.9.4-1/bin:$PATH"
+if [[ ! "$PATH" =~ "/usr/local/Cellar/fzf/0.9.7/bin" ]]; then
+  export PATH="$PATH:/usr/local/Cellar/fzf/0.9.7/bin"
+fi
+
+# Man path
+# --------
+if [[ ! "$MANPATH" =~ "/usr/local/Cellar/fzf/0.9.7/man" && -d "/usr/local/Cellar/fzf/0.9.7/man" ]]; then
+  export MANPATH="$MANPATH:/usr/local/Cellar/fzf/0.9.7/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- =~ i ]] && source "/usr/local/Cellar/fzf/0.9.4-1/fzf-completion.zsh"
+[[ $- =~ i ]] && source "/usr/local/Cellar/fzf/0.9.7/shell/completion.zsh"
 
 # Key bindings
 # ------------
-# CTRL-T - Paste the selected file path(s) into the command line
-__fsel() {
-  command find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-    -o -type f -print \
-    -o -type d -print \
-    -o -type l -print 2> /dev/null | sed 1d | cut -b3- | fzf -m | while read item; do
-    printf '%q ' "$item"
-  done
-  echo
-}
+source "/usr/local/Cellar/fzf/0.9.7/shell/key-bindings.zsh"
 
-if [[ $- =~ i ]]; then
-
-if [ -n "$TMUX_PANE" -a ${FZF_TMUX:-1} -ne 0 -a ${LINES:-40} -gt 15 ]; then
-  fzf-file-widget() {
-    local height
-    height=${FZF_TMUX_HEIGHT:-40%}
-    if [[ $height =~ %$ ]]; then
-      height="-p ${height%\%}"
-    else
-      height="-l $height"
-    fi
-    tmux split-window $height "zsh -c 'source ~/.fzf.zsh; tmux send-keys -t $TMUX_PANE \"\$(__fsel)\"'"
-  }
-else
-  fzf-file-widget() {
-    LBUFFER="${LBUFFER}$(__fsel)"
-    zle redisplay
-  }
-fi
-zle     -N   fzf-file-widget
-bindkey '^T' fzf-file-widget
-
-# ALT-C - cd into the selected directory
-fzf-cd-widget() {
-  cd "${$(command find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-    -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m):-.}"
-  zle reset-prompt
-}
-zle     -N    fzf-cd-widget
-bindkey '\ec' fzf-cd-widget
-
-# CTRL-R - Paste the selected command from history into the command line
-fzf-history-widget() {
-  LBUFFER=$(fc -l 1 | fzf +s --tac +m -n2..,.. | sed "s/ *[0-9*]* *//")
-  zle redisplay
-}
-zle     -N   fzf-history-widget
-bindkey '^R' fzf-history-widget
-
-fi
