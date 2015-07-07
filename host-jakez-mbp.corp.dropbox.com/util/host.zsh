86f40fafbc589125447a15071c6c5fad3bf1c4ba
Jacob Zimmerman <zimmerman.jake@gmail.com>
2015-07-07 10:33:44 -0700

    Filter fzf through ag first

--- host-jakez-mbp.corp.dropbox.com/util/host.zsh
+++ host-jakez-mbp.corp.dropbox.com/util/host.zsh
@@ -1,5 +1,7 @@
 
 # fzf fuzzy file finding
+export FZF_DEFAULT_COMMAND='ag -l -g ""'
+export FZF_CTRL_T_COMMAND='ag -l -g ""'
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 # Modify keydings to match ctrlp.vim
 bindkey '^P' fzf-file-widget
