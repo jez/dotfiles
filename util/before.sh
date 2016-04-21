316587cb1f5b2f546c38c72a6130eebc60786392
Jacob Zimmerman <zimmerman.jake@gmail.com>
2016-04-21 18:56:56 -0400

    shell: Initialize some XDG environment vars

--- util/before.sh
+++ util/before.sh
@@ -9,3 +9,5 @@ export PATH=".:$HOME/bin:/usr/local/bin:$PATH"
 
 # set XDG Base Directories appropriately
 export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
