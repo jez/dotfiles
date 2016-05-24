5115ecacfc9b8d1e50cd099a933ed77743ed758d
Jake Zimmerman <jzimmerman@stripe.com>
2016-05-23 20:50:49 -0700

    misc: New laptop setup for Stripe

---
+++ host-st-jzimmerman1.local/util/host.sh
@@ -0,0 +1,56 @@
+#
+# host.sh - Host-dependent configurations for "st-jzimmerman1.local":
+#           Stripe-specific config
+#
+# Author
+#   Jake Zimmerman <jake@zimmerman.io>
+#
+# Usage
+#   Source this file.
+#
+# Notes
+#   Defines `update-host` which is called by auto-update.sh's `update`
+#   function. Make sure to source this file before it.
+#
+# TODOs
+#   - This file isn't as modular as it could be.
+
+# ----- PATH and MANPATH ------------------------------------------------------
+
+# Use GNU coreutils with their actual names
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
+# ----- aliases ---------------------------------------------------------------
+
+# Double double letters are hard
+which coffee &> /dev/null && alias coffe="coffee"
+
+# Use neovim for the lulz
+alias vim="nvim -p"
+
+# Override BSD grep with GNU equivalent
+which ggrep &> /dev/null && alias grep="ggrep --color=auto";
+
+# ----- Prompt  ---------------------------------------------------------------
+
+export PROMPT_PURE_DIR_COLOR="%{$cmagentab%}"
+
+# ----- Miscellaneous ---------------------------------------------------------
+
+# update-host
+# Usage:
+#     update-host
+#
+# You should never have to call this. It's called when you call `update`
+update-host () {
+  echo "$cblueb==>$cwhiteb Updating Homebrew...$cnone"
+  brew update
+
+  echo "$cblueb==>$cwhiteb Checking Homebrew...$cnone"
+  brew doctor
+
+  echo "$cblueb==>$cwhiteb Checking for outdated brew packages...$cnone"
+  brew outdated --verbose
+
+}
