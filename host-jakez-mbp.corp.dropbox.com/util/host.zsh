3c9445804502bee5d30c64772a2bc6814f7d2dfd
Jake Zimmerman <jzimmerman@stripe.com>
2016-06-01 14:42:32 -0700

    fzf: Update fzf color scheme

--- host-jakez-mbp.corp.dropbox.com/util/host.zsh
+++ host-jakez-mbp.corp.dropbox.com/util/host.zsh
@@ -43,12 +43,12 @@ _gen_fzf_default_opts() {
 
   # Solarized Dark color scheme for fzf
   export FZF_DEFAULT_OPTS="
-    --color fg:-1,bg:-1,hl:$blue,fg+:$base2,bg+:$base02,hl+:$blue
+    --color fg:-1,bg:-1,hl:$blue,fg+:$base2,bg+:-1,hl+:$blue
     --color info:$yellow,prompt:$yellow,pointer:$base3,marker:$base3,spinner:$yellow
   "
   ## Solarized Light color scheme for fzf
   #export FZF_DEFAULT_OPTS="
-  #  --color fg:-1,bg:-1,hl:$blue,fg+:$base02,bg+:$base2,hl+:$blue
+  #  --color fg:-1,bg:-1,hl:$blue,fg+:$base02,bg+:-1,hl+:$blue
   #  --color info:$yellow,prompt:$yellow,pointer:$base03,marker:$base03,spinner:$yellow
   #"
 }
