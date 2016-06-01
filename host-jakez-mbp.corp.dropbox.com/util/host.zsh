b6247028d844045f8edcaca5c660a86c9bd2f33f
Jake Zimmerman <jzimmerman@stripe.com>
2016-05-31 22:57:10 -0700

    fzf: Move Solarized Light colorscheme to two lines

--- host-jakez-mbp.corp.dropbox.com/util/host.zsh
+++ host-jakez-mbp.corp.dropbox.com/util/host.zsh
@@ -48,7 +48,8 @@ _gen_fzf_default_opts() {
   "
   ## Solarized Light color scheme for fzf
   #export FZF_DEFAULT_OPTS="
-  #  --color fg:-1,bg:-1,hl:$blue,fg+:$base02,bg+:$base2,hl+:$blue,info:$yellow,prompt:$yellow,pointer:$base03,marker:$base03,spinner:$yellow
+  #  --color fg:-1,bg:-1,hl:$blue,fg+:$base02,bg+:$base2,hl+:$blue
+  #  --color info:$yellow,prompt:$yellow,pointer:$base03,marker:$base03,spinner:$yellow
   #"
 }
 _gen_fzf_default_opts
