b11ee552d2272f0708c9d4986c18a168c569a597
Jacob Zimmerman <zimmerman.jake@gmail.com>
2016-09-17 13:23:33 -0400

    shell: Use node_modules bin unprefixed

--- host-Jacobs-MacBook-Air/util/host.sh
+++ host-Jacobs-MacBook-Air/util/host.sh
@@ -20,6 +20,9 @@
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
+# Use executables installed to a project by npm
+export PATH="$PATH:node_modules/.bin"
+
 # Add diff-highlight from Git contrib
 export PATH="$PATH:/usr/local/opt/git/share/git-core/contrib/diff-highlight"
 
