0bedf08092e7606480b139b43e8024fababd4b79
Jacob Zimmerman <zimmerman.jake@gmail.com>
2017-07-07 21:42:45 -0500

    shell: Update fzf

--- host-Jacobs-MacBook-Air/fzf.zsh
+++ host-Jacobs-MacBook-Air/fzf.zsh
@@ -4,12 +4,6 @@ if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
   export PATH="$PATH:/usr/local/opt/fzf/bin"
 fi
 
-# Man path
-# --------
-if [[ ! "$MANPATH" == */usr/local/opt/fzf/man* && -d "/usr/local/opt/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/usr/local/opt/fzf/man"
-fi
-
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
