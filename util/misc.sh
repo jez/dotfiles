c360be37372de684f1a21d32e9ca28cf6ff4f8ab
Jacob Zimmerman <zimmerman.jake@gmail.com>
2017-07-13 13:58:31 -0500

    tmux: Fix idiosyncrasies with italics

--- util/misc.sh
+++ util/misc.sh
@@ -11,11 +11,12 @@ which dircolors &> /dev/null && eval $(dircolors ~/.dircolors)
 # now, when we us `ls` we'll get solarized-colored files and directories!
 
 # Turn on italics
-tic ~/.xterm-256color-italic.terminfo
-tic ~/.screen-256color-italic.terminfo
-
-if ! echo $TERM | grep 'italic' &> /dev/null; then
-  export TERM=xterm-256color-italic
+if echo $TERM | grep 'tmux' &> /dev/null; then
+  tic ~/.tmux-256color.terminfo
+elif echo $TERM | grep 'xterm' &> /dev/null; then
+  tic ~/.xterm-256color.terminfo
+else
+  export TERM=xterm-256color
 fi
 # Things will break if you don't have these terminfo changes applied on _every_
 # host that you use; i.e., every box that you ssh into
