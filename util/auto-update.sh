aa884cd01f92aa47d33ea712ece4d46089f48ebb
Jake Zimmerman <zimmerman.jake@gmail.com>
2017-08-25 15:31:52 -0700

    shell: Refactor auto-update into function

--- util/auto-update.sh
+++ util/auto-update.sh
@@ -41,29 +41,33 @@ esac
 # Number of seconds to wait before printing a reminder
 UPDATE_THRESHOLD="86400"
 
-[ ! -e $HOME/.last_update ] && touch $HOME/.last_update
-# Initialize for when we have no GNU date available
-last_check=0
-time_now=0
+check_for_updates() {
+  [ ! -e $HOME/.last_update ] && touch $HOME/.last_update
+  # Initialize for when we have no GNU date available
+  last_check=0
+  time_now=0
 
-# Darwin uses BSD, check for gdate, else use date
-if [[ $(uname) = "Darwin" && -n $(which gdate) ]]; then
-  last_login=$(gdate -r ~/.last_update +%s)
-  time_now=$(gdate +%s)
-else
-  # Ensure this is GNU grep
-  if [ -n "$(date --version 2> /dev/null | grep GNU)" ]; then
-    last_login=$(date -r ~/.last_update +%s)
-    time_now=$(date +%s)
+  # Darwin uses BSD, check for gdate, else use date
+  if [[ $(uname) = "Darwin" && -n $(which gdate) ]]; then
+    last_login=$(gdate -r ~/.last_update +%s)
+    time_now=$(gdate +%s)
+  else
+    # Ensure this is GNU grep
+    if [ -n "$(date --version 2> /dev/null | grep GNU)" ]; then
+      last_login=$(date -r ~/.last_update +%s)
+      time_now=$(date +%s)
+    fi
   fi
-fi
 
-time_since_check=$((time_now - last_login))
+  time_since_check=$((time_now - last_login))
 
-if [ "$time_since_check" -ge "$UPDATE_THRESHOLD" ]; then
-  echo "$cred==>$cemph Your system is out of date!$cnone"
-  echo 'Run `update` to bring it up to date.'
-fi
+  if [ "$time_since_check" -ge "$UPDATE_THRESHOLD" ]; then
+    echo "$cred==>$cemph Your system is out of date!$cnone"
+    echo 'Run `update` to bring it up to date.'
+  fi
+}
+
+check_for_updates
 
 # update - fetch updates
 # usage:
