5cda64644a5ef9400851c9ea5f0fe9719a4649eb
Jacob Zimmerman <zimmerman.jake@gmail.com>
2017-09-02 19:46:36 -0700

    shell: Commit gnupg file

---
+++ util/gnupg.sh
@@ -0,0 +1,11 @@
+#
+# gnupg.sh - GNU Privacy Guard configuration
+#
+# Author
+#   Jake Zimmerman <jake@zimmerman.io>
+#
+# Usage
+#   Source this file.
+#
+
+export GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
