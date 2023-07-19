8e7fbf6b3df0eb0d9a53ab8b69103fe562aca74e
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-07-19 16:04:04 -0700

    Revert "shell: devbox-setup changes"
    This reverts commit a35f16f5308d20b67f935da83a53743d76bfb3ce.

--- devbox-setup.sh
+++ devbox-setup.sh
@@ -24,6 +24,7 @@ git commit-graph write --reachable
 echo 'Fixing /pay/home mount point for Linuxbrew'
 
 sudo unlink /home
+sudo mkdir /home
 sudo mount --bind /pay/home /home
 echo "/pay/home /home none bind 0 0" | sudo tee -a /etc/fstab
 
