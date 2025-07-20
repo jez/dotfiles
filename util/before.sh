284448e07c0298e64dd0b3fd91a0b8a4f4a8b4f7
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-07-20 11:47:49 -0700

    Remove greatwhite hack from before.sh

--- util/before.sh
+++ util/before.sh
@@ -1,9 +1,6 @@
 
 # hacks to run before everything else
 
-# greatwhite.ics.cs.cmu.edu is far too old for anything I'd want to do
-[[ $(hostname) =~ greatwhite ]] && exit
-
 # set XDG Base Directories appropriately
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
