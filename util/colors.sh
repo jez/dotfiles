24ca6265378243a6bafb55a23aa9eaab546c0159
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-07-20 12:17:37 -0700

    shell: Optimize colors.sh performance

--- util/colors.sh
+++ util/colors.sh
@@ -9,37 +9,37 @@
 #   This works anywhere that shell variables are expanded.
 
 # ANSI colors
-export cnone="$(echo -ne '\033[0m')"
-export cwhiteb="$(echo -ne '\033[1;37m')"
-export cwhite="$(echo -ne '\033[0;37m')"
-export cgray="$(echo -ne '\033[1;30m')"
-export cblack="$(echo -ne '\033[0;30m')"
+export cnone=$'\x1b[0m'
+export cwhiteb=$'\x1b[1;37m'
+export cwhite=$'\x1b[0;37m'
+export cgray=$'\x1b[1;30m'
+export cblack=$'\x1b[0;30m'
 
-export cred="$(echo -ne '\033[0;31m')"
-export credb="$(echo -ne '\033[1;31m')"
-export cgreen="$(echo -ne '\033[0;32m')"
-export cgreenb="$(echo -ne '\033[1;32m')"
-export cyellow="$(echo -ne '\033[0;33m')"
-export cyellowb="$(echo -ne '\033[1;33m')"
-export cblue="$(echo -ne '\033[0;34m')"
-export cblueb="$(echo -ne '\033[1;34m')"
-export cmagenta="$(echo -ne '\033[0;35m')"
-export cmagentab="$(echo -ne '\033[1;35m')"
-export ccyan="$(echo -ne '\033[0;36m')"
-export ccyanb="$(echo -ne '\033[1;36m')"
+export cred=$'\x1b[0;31m'
+export credb=$'\x1b[1;31m'
+export cgreen=$'\x1b[0;32m'
+export cgreenb=$'\x1b[1;32m'
+export cyellow=$'\x1b[0;33m'
+export cyellowb=$'\x1b[1;33m'
+export cblue=$'\x1b[0;34m'
+export cblueb=$'\x1b[1;34m'
+export cmagenta=$'\x1b[0;35m'
+export cmagentab=$'\x1b[1;35m'
+export ccyan=$'\x1b[0;36m'
+export ccyanb=$'\x1b[1;36m'
 
 # Solarized 256 colors
-export sred="$(echo -ne '\033[00;38;5;160m')"
-export sorange="$(echo -ne '\033[00;38;5;166m')"
-export syellow="$(echo -ne '\033[00;38;5;136m')"
-export sgreen="$(echo -ne '\033[00;38;5;64m')"
-export sblue="$(echo -ne '\033[00;38;5;33m')"
-export spurple="$(echo -ne '\033[00;38;5;61m')"
-export sbgray="$(echo -ne '\033[01;38;5;245m')"
-export slgray="$(echo -ne '\033[00;38;5;244m')"
-export sfaint="$(echo -ne '\033[00;38;5;240m')"
-export swhite="$(echo -ne '\033[00;38;5;15m')"
-export sreset="$(echo -ne '\033[0m')"
+export sred=$'\x1b[00;38;5;160m'
+export sorange=$'\x1b[00;38;5;166m'
+export syellow=$'\x1b[00;38;5;136m'
+export sgreen=$'\x1b[00;38;5;64m'
+export sblue=$'\x1b[00;38;5;33m'
+export spurple=$'\x1b[00;38;5;61m'
+export sbgray=$'\x1b[01;38;5;245m'
+export slgray=$'\x1b[00;38;5;244m'
+export sfaint=$'\x1b[00;38;5;240m'
+export swhite=$'\x1b[00;38;5;15m'
+export sreset=$'\x1b[0m'
 
 # Utility function to pick any xterm-256 color
 # Usage:
