986aba74f199afa6160cc66b9ae0d2f966cd8efd
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-08-31 21:54:54 -0700

    Initial host support for jez-laptop-02

---
+++ host-jez-raspberry-pi-03/util/host.vim
@@ -0,0 +1,1 @@
+set runtimepath+=$HOME/.local/opt/fzf
---
+++ host-jez-raspberry-pi-03/util/host.zsh
@@ -0,0 +1,12 @@
+#
+# host.zsh - Host-specific, zsh-specific configurations
+#
+# Author
+#   Jake Zimmerman <jake@zimmerman.io>
+#
+# Usage
+#   Source this file.
+
+
+load_module ~/.util/fzf.zsh
+load_module ~/.util/bazel.zsh
